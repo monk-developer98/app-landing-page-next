f3771ee98c315e4485eecf3d35a573e005849059
Ashish Saxena <ashish.sxn847@gmail.com>
2025-02-04 23:09:47 +0530

    Review Section Done

--- components/Home/Home.tsx
+++ components/Home/Home.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 import Hero from './Hero/Hero'
 import WhyChoose from './WhyChoose/WhyChoose'
 import AnalyticsFeature from './Analytic/AnalyticsFeature'
+import Feature from './Feature/Feature'
+import Review from './Review/Review'
 
 const Home = () => {
   return (
@@ -9,6 +11,8 @@ const Home = () => {
       <Hero />
       <WhyChoose/>
       <AnalyticsFeature/>
+      <Feature/>
+      <Review/>
     </div>
   )
 }
